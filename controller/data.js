12df59be3e49a363b2b46c1f105850c81dd98325
Aure31880 <barbotin48@gmail.com>
2022-01-17 22:24:09 +0100

    finish delete function & add converter to xml

--- controller/data.js
+++ controller/data.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Data = require('../model/Data.js');
+var convert = require('xml-js');
 const AxiosService = require('../services/AxiosService');
 
 exports.getAndSaveFile = (req, res, next) => {
@@ -7,7 +8,6 @@ exports.getAndSaveFile = (req, res, next) => {
         .then(result => {
             const dataFetch = result.data;
             const dataString = JSON.stringify(dataFetch);
-            console.log(dataString);
             const data = new Data({
                 idData: dataFetch.id,
                 file: dataString
@@ -15,33 +15,52 @@ exports.getAndSaveFile = (req, res, next) => {
 
             data.save()
                 .then(() => res.status(201).json({ message: 'Requète reçue !' }))
-                .catch(error => res.status(400).json(error));
+                .catch(error => res.status(401).json(error));
         })
-
 }
 
 exports.getFiles = (req, res, next) => {
     Data.findOne({ id: req.params.idData })
-        .then(file => res.status(200).json(file))
+        .then(file => {
+            if (!file) {
+                return res.status(400).json('Aucun fichier !')
+            } else {
+                return res.status(200).json(file)
+            }
+        })
         .catch(error => res.status(400).json(error));
 }
 
 exports.updateFile = (req, res, next) => {
     Data.updateOne({ _id: req.params.idData }, { ...req.body, _id: req.params.idData })
         .then(() => res.status(200).json({ message: 'File modifié !' }))
-        .catch(error => res.status(400).json({ error }))
+        .catch(error => res.status(404).json(error))
 }
 
 exports.deleteFile = (req, res, next) => {
     Data.findOne({ id: req.params.idData })
         .then(result => {
-            console.log(result);
             if (!result) {
                 return res.status(404).json({ message: "Le fichier n'existe pas" })
             }
             Data.deleteOne({ id: req.params.idData })
                 .then(() => res.status(201).json({ message: 'Fichier supprimer !' }))
-                .catch(error => res.status(400).json({ error }));
+                .catch(error => res.status(404).json(error));
         })
         .catch(error => res.status(500).json(error))
 }
+
+exports.getFileJsonToXml = (req, res, next) => {
+    Data.findOne({ id: req.params.idData })
+        .then(file => {
+            if (!file) {
+                return res.status(404).json(new Error('Aucun fichier !'))
+            }
+            const dataToConvert = file.file;
+            var options = { compact: true, ignoreComment: true, spaces: 4 };
+            const resultConvert = convert.json2xml(dataToConvert, options);
+            return res.status(200).send(resultConvert)
+        })
+        .catch(error => res.status(400).json(error));
+}
+
--- services/AxiosService.js
+++ services/AxiosService.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 exports.getFiles = async (req, res, next) => {
     return await axios.get('https://magellan-okgo-demo.osc-fr1.scalingo.io/files/ao1.json', {
-        // note the use of querystring
         headers: {
             'Content-Type': 'application/json; charset=UTF-8',
             'Authorization': process.env.TOKEN
